636b93e8132d9769473ef6efbd7889b65a230eba
MecIntyre <abuesswalker@gmail.com>
2021-06-29 14:58:07 +0200

    Default-Route auf rt-b und DNS + bootstrap

--- Vagrantfile
+++ Vagrantfile
@@ -2,13 +2,14 @@
 
 Vagrant.configure("2") do |config|
   config.vm.box = "ubuntu/focal64"
+  config.vm.provision "shell", path: "scripts/bootstrap.sh"
   config.vm.define "docker1" do |docker1|
     docker1.vm.hostname = "docker1"
     docker1.vm.network "private_network", ip: "192.168.100.100"
     docker1.vm.provider "virtualbox" do |vb|
       vb.name = "docker1"
       vb.cpus = "2"
-      vb.memory = "4096"
+      vb.memory = "1024"
     end
     docker1.vm.provision "shell", path: "scripts/docker-provision.sh"
   end
--- scripts/docker-provision.sh
+++ scripts/docker-provision.sh
@@ -3,10 +3,17 @@
 # APT im nicht-interaktiven Modus 
 export DEBIAN_FRONTEND=noninteractiv
 
-# Systemaktualisierung und löschen von nutzlosen Datein
-apt-get update && apt-get -y dist-upgrade && apt-get --purge -y autoremove
-
 # Docker installieren
 apt-get -y install docker.io
 systemctl enable --now docker
 adduser vagrant docker
+
+# Default Route setzen
+ip route replace default via 192.168.100.2
+ip route del default via 10.0.2.2
+
+# Konfiguration der Namensauflösung
+systemctl disable --now systemd-resolved
+rm /etc/resolv.conf
+echo "nameserver 192.168.100.2" > /etc/resolv.conf
+echo "search kurs.iad" >> /etc/resolv.conf
